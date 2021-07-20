605f1076e088cf78c6581a4d7f07be1f82957f45
Ben lin <benjaminlinhappy@gmail.com>
2021-07-20 15:42:24 +0800

    'count

--- guess.py
+++ guess.py
@@ -1,12 +1,15 @@
 import random
 r = random.randint(1,100)
+count = 0
 while True:
+	count += 1
 	num = int(input('guess a number:'))
 	if num == r:
 		print('you are right!')
+		print('you guessed', count, 'times in total')
 		break
 	elif num > r:
 		print('too big')
 	elif num < r:
 		print('too small')
-		
+	print('this is your', count, 'guess')
